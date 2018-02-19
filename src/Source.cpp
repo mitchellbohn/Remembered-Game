51929861b19e54bfdd871601b2c25665f7467c31
Mitchell Bohn <mwbohn@mavs.coloradomesa.edu>
2018-02-19 13:34:53 -0700

    empty

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,39 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <string>
+#include <sstream>
+
 #include <SDL.h>
-#include <stdio.h>
 
-const int SCREEN_WIDTH = 1920;
-const int SCREEN_HEIGHT = 1080;
+#define DEBUG(s) //cerr << s << endl;
 
-int main( int argc, char* args[] )
-{
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
+using namespace std;
 
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			screenSurface = SDL_GetWindowSurface( window );
 
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			SDL_UpdateWindowSurface( window );
+class ProtoGame {
+	
+};
 
-			SDL_Delay( 2000 );
-		}
-	}
+class AnimationFrame {
+	
+};
 
-	SDL_DestroyWindow( window );
-	SDL_Quit();
+class Sprite {
 
+};
+
+class Ball:public Sprite {
+
+};
+
+class Game:public ProtoGame {
+	
+};
+
+class TheGame: public Game {
+	
+};
+
+TheGame MyGame;
+
+int main(int argc, char* argv[]) {
+	MyGame.init("MyGame", 640, 480);
+	DEBUG("After init ");
+	MyGame.run();
+	MyGame.exit();
 	return 0;
 }
